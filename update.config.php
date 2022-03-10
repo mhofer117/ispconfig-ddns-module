89eba26ecfbf6997e1f8d5f5c4fb4737e0d324ca
Marcel Hofer <m.hofer117@gmail.com>
2022-03-10 23:12:24 +0100

    fix default forwarded header name

--- update.config.php
+++ update.config.php
@@ -4,5 +4,5 @@ return array(
     'TRUSTED_PROXY_KEY' => '',
     'TRUSTED_PROXY_IP' => '127.0.0.1',
     'PROXY_KEY_HEADER' => 'X_DDNS_PROXY_KEY',
-    'PROXY_IP_HEADER' => 'X_FORWARED_FOR'
+    'PROXY_IP_HEADER' => 'X_FORWARDED_FOR'
 );
