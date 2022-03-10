16ae62671b58b48532979c609e82cdb830c26175
Marcel Hofer <m.hofer117@gmail.com>
2022-03-10 22:27:23 +0100

    fix include path

--- update.php
+++ update.php
@@ -4,9 +4,9 @@ require_once(dirname(__FILE__) . '/../../lib/app.inc.php');
 require_once(dirname(__FILE__) . '/lib/updater/DdnsUpdater.php');
 ini_set('session.use_cookies', '0');
 
-$default_config = include dirname(__FILE__) . 'update.config.php';
-if (file_exists(dirname(__FILE__) . 'update.config.local.php')) {
-    $config_local = include dirname(__FILE__) . 'update.config.local.php';
+$default_config = include dirname(__FILE__) . '/update.config.php';
+if (file_exists(dirname(__FILE__) . '/update.config.local.php')) {
+    $config_local = include dirname(__FILE__) . '/update.config.local.php';
     $config = array_merge($default_config, $config_local);
 } else {
     $config = $default_config;
