e39de09bf1808c6f73efb7a5b86e96a3089c20eb
Marcel Hofer <m.hofer117@gmail.com>
2022-03-11 20:23:27 +0100

    show proxy_host in ui URLs

--- ddns_token_list.php
+++ ddns_token_list.php
@@ -21,6 +21,16 @@ $auth->check_module_permissions('dns');
 
 $app->uses('listform_actions');
 
+// load custom config for PROXY_HOST variable
+$default_config = array('PROXY_HOST' => '');
+if (file_exists(dirname(__FILE__) . '/update.config.local.php')) {
+    $config_local = include dirname(__FILE__) . '/update.config.local.php';
+    $config = array_merge($default_config, $config_local);
+} else {
+    $config = $default_config;
+}
+$app->tpl->setVar('PROXY_HOST', $default_config['PROXY_HOST']);
+
 // $app->listform_actions->SQLExtWhere = "dns_soa.access = 'REJECT'";
 // $app->listform_actions->SQLOrderBy = 'ORDER BY dns_soa.origin';
 
